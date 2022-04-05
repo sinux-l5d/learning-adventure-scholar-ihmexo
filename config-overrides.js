4151bd67867888aa940e10861edda5b31d445f09
JediSim <simpiccc@gmail.com>
2022-04-05 08:56:23 +0200

    refactor: modification config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,8 @@
 const path = require("path");
-// const { alias } = require("react-app-rewire-alias");
 const { alias, aliasJest } = require("react-app-rewire-alias");
 
-// module.exports = function override(config, env) {
-//   alias({
-//     "@": "./src/",
-//     "@components": "./src/components/",
-//     "@pages": "./src/pages",
-//     "@services": "./src/services/",
-//     "@stores": "./src/services/stores/",
-//     //   routes: path.resolve(__dirname, "src/routes"),
-//   })(config);
-
-//   return config;
-// };
-
 const aliasMap = {
-  "@": "./src/",
+  // "@": "./src/",
   "@components": "./src/components/",
   "@pages": "./src/pages",
   "@services": "./src/services/",
