c45ee67d72b82dc91303f2fd7d8d5ff2c195da5f
TheSummer1502 <pajeanromain@gmail.com>
2022-05-16 19:25:29 +0200

    fix: FromSeanceCard avec Grid

--- src/components/FormCreerSession/FormSeanceCard.js
+++ src/components/FormCreerSession/FormSeanceCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Paper, Box, TextField } from '@mui/material';
+import { Paper, Box, Grid, TextField } from '@mui/material';
 import GroupsIcon from '@mui/icons-material/Groups';
 import SchoolIcon from '@mui/icons-material/School';
 
@@ -13,23 +13,30 @@ const FormSeanceCard = (props) => {
 
   return (
     <Paper sx={{ padding: '2em' }}>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <GroupsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField
-          id="groupe-seance"
-          label="Groupe"
-          variant="standard"
-          sx={{ width: '48%', marginRight: '4%' }}
-        />
-
-        <SchoolIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField
-          id="encadrant-seance"
-          label="Encadrant"
-          variant="standard"
-          sx={{ width: '48%' }}
-        />
-      </Box>
+      <Grid container columnSpacing={10} rowSpacing={2}>
+        <Grid item md={6} sm={12} xs={12}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <GroupsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField
+              id="groupe-seance"
+              label="Groupe"
+              variant="standard"
+              sx={{ width: '100%' }}
+            />
+          </Box>
+        </Grid>
+        <Grid item md={6} sm={12} xs={12}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <SchoolIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField
+              id="encadrant-seance"
+              label="Encadrant"
+              variant="standard"
+              sx={{ width: '100%' }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
